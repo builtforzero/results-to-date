b9da85dcff0f479ce557dbe1da241517885547e4
koffeeya <kavya.beheraj@gmail.com>
2020-11-12 14:04:59 -0500

    removed tabletop js dependency

--- client/main.js
+++ client/main.js
@@ -1,143 +1,180 @@
+require('dotenv').config();
+const d3 = require("d3");
+const Papa = require("papaparse");
+const geoData = require("../assets/usState.json");
+import 'regenerator-runtime/runtime'
+
 // Set global state
 let state = {
   geo: null,
-  results: [],
-  mapData: [],
   communities: 0,
   vetEnded: 0,
   reduction: 0,
   housed: 0,
   qualityData: 0,
   listActive: false,
+  apiKey: process.env.API_KEY,
+  results: null,
+  map: null,
+}
+
+let results = {
+  spreadsheetId: process.env.RESULTS_SPREADSHEET_ID,
+  range: process.env.RESULTS_RANGE,
+  data: null
+}
+
+let map = {
+  spreadsheetId: process.env.MAP_SPREADSHEET_ID,
+  range: process.env.MAP_RANGE,
+  data: null
 }
 
 let svg, path, projection;
 
-// URLs of public Google Sheets with data
-let resultsURL = 'https://docs.google.com/spreadsheets/d/1vr2jahJzoSfdekaPwzNDJ06VueEF1wIqVOcABH6bCdU/edit#gid=1274736575';
-let mapURL = 'https://docs.google.com/spreadsheets/d/1lH3o8LHp3a-s2p004jmTTsEjLeHgt-D-E8xAZAC2eow/edit#gid=1665190279';
+function getResultsData(apiKey, spreadsheetId, range) {
+  const getUrl = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${range}?key=${apiKey}`;
+  const isUndefined = (element) => element === undefined;
+  const config = [spreadsheetId, range, apiKey]
 
-// Load data in three steps using Tabletop.js
-function init() {
-
-  // Step 1: Load results data, then call map data loader
-  Tabletop.init({
-    key: resultsURL,
-    callback: function (data, tabletop) {
-      state.results = data;
-      loadMap();
-    },
-    simpleSheet: true
-  });
-
-  // Step 2: Load map data, then call function to set map projection and scales
-  // Sort by veteran functional zero date to show those dots on top
-  function loadMap() {
-    Tabletop.init({
-      key: mapURL,
-      callback: function (data, tabletop) {
-        state.mapData = data.sort((a, b) => {
-          return d3.ascending(a.community, b.community)
-        });
-        setMapAndScales(state);
-      },
-      simpleSheet: true
+  if (config.some(isUndefined)) {
+    console.log("Config Values Undefined!", config)
+  } else {
+    fetch(getUrl).then(function(response) {
+       return response.json();
+    }).then(function (data) {
+      const csv = Papa.unparse(data.values, {
+        header: true,
+        quotes: true
+      })
+      const json = Papa.parse(csv, {
+          header: true,
+          dynamicTyping: true,
+      })
+      getMapData(state.apiKey, map.spreadsheetId, map.range, json.data)
     })
+    .catch(error => console.error('Error!', error));
   }
+}
 
+function getMapData(apiKey, spreadsheetId, range, resultData) {
+  results.data = resultData;
+  const getUrl = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${range}?key=${apiKey}`;
+  const isUndefined = (element) => element === undefined;
+  const config = [spreadsheetId, range, apiKey]
+
+  if (config.some(isUndefined)) {
+    console.log("Config Values Undefined!", config)
+  } else {
+    fetch(getUrl).then(function(response) {
+       return response.json();
+    }).then(function (data) {
+      const csv = Papa.unparse(data.values, {
+        header: true,
+        quotes: true
+      })
+      const json = Papa.parse(csv, {
+          header: true,
+          dynamicTyping: true,
+      })
+      setMapAndScales(state, results, json.data);
+    })
+    .catch(error => console.error('Error!', error));
+  }
+}
+
+
+function init() {
+  getResultsData(state.apiKey, results.spreadsheetId, results.range)
 }
 
 init();
 
-// Step 3: Set map projection and get topline numbers, then call draw
-function setMapAndScales(state) {
-  Promise.all([
-    d3.json("./data/usState.json"),
-  ]).then(([usaData]) => {
-    state.geo = usaData;
+function setMapAndScales(state, results, mapData) {
+  map.data = mapData;
+  state.geo = geoData;
+  projection = d3.geoAlbersUsa().fitSize([1059, 625], state.geo);
+  path = d3.geoPath().projection(projection);
 
-    // Use AlbersUSA projection, scale to canvas and geography
-    projection = d3.geoAlbersUsa().fitSize([1059, 625], state.geo);
-    path = d3.geoPath().projection(projection);
+  state.communities = results.data.filter(d => {
+    return d.Category === "communities"
+  })[0].Value;
+  state.vetEnded = results.data.filter(d => {
+    return d.Category === "vetEnded"
+  })[0].Value;
+  state.reduction = results.data.filter(d => {
+    return d.Category === "reduction"
+  })[0].Value;
+  state.housed = results.data.filter(d => {
+    return d.Category === "housed"
+  })[0].Value;
+  state.qualityData = results.data.filter(d => {
+    return d.Category === "qualityData"
+  })[0].Value;
 
-    // Filter results data for topline numbers and assign to state
-    state.communities = state.results.filter(d => {
-      return d.Category === "communities"
-    })[0].Value;
-    state.vetEnded = state.results.filter(d => {
-      return d.Category === "vetEnded"
-    })[0].Value;
-    state.reduction = state.results.filter(d => {
-      return d.Category === "reduction"
-    })[0].Value;
-    state.housed = state.results.filter(d => {
-      return d.Category === "housed"
-    })[0].Value;
-    state.qualityData = state.results.filter(d => {
-      return d.Category === "qualityData"
-    })[0].Value;
-
-    draw(state);
-
-  });
-
+  draw(state, map, results);
 }
 
 // Set subtitle in tooltip if chronic / vet functional zero date exists
 function setSubtitle(vetDate, chronicDate) {
-  if (vetDate == "" && chronicDate == "") {
+  if (vetDate == undefined && chronicDate == undefined) {
     return "";
-  } else if (vetDate != "" && chronicDate == "") {
+  } else if (vetDate != undefined && chronicDate == undefined) {
     return `&nbspEnded <b>veteran</b> homelessness in <b>${vetDate}</b>&nbsp`;
-  } else if (chronicDate != "" && vetDate == "") {
+  } else if (chronicDate != undefined && vetDate == undefined) {
     return `&nbspEnded <b>chronic</b> homelessness in <b>${chronicDate}</b>&nbsp`;
-  } else if (vetDate != "" && chronicDate != "") {
+  } else if (vetDate != undefined && chronicDate != undefined) {
     return `&nbspEnded <b>veteran</b> homelessness in <b>${vetDate}</b>&nbsp<br>&nbspEnded <b>chronic</b> homelessness in <b>${chronicDate}</b>&nbsp`;
   }
 }
 
+function setTooltip(community, state, vetDate, chronicDate) {
+  const cleanCommunity = community.replace(" CoC", "").replace(" Regional", "").replace("Countys", "County");
+  return `<b>${cleanCommunity}</b>, <b style='font-weight:400;'> ${state}</b> <br><b style='font-size: 12px; font-weight:400;'> ${setSubtitle(vetDate, chronicDate)} </b>`
+}
+
 function setAsterisk(vetDate, chronicDate) {
-  if (vetDate == "" && chronicDate == "") {
+  if (vetDate == undefined && chronicDate == undefined) {
     return "";
-  } else if (vetDate != "" && chronicDate == "") {
+  } else if (vetDate != undefined && chronicDate == undefined) {
     return `&nbsp &#x2605;
     &nbsp`;
-  } else if (chronicDate != "" && vetDate == "") {
+  } else if (chronicDate != undefined && vetDate == undefined) {
     return `&nbsp &#x2605;
     &nbsp`;
-  } else if (vetDate != "" && chronicDate != "") {
+  } else if (vetDate != undefined && chronicDate != undefined) {
     return `&nbsp &#x2605;
     &nbsp`;
   }
 }
 
 function setSubtitleColor(vetDate, chronicDate) {
-  if (vetDate == "" && chronicDate == "") {
+  if (vetDate == undefined && chronicDate == undefined) {
     return "rgb(22, 22, 22)";
-  } else if (vetDate != "" && chronicDate == "") {
+  } else if (vetDate != undefined && chronicDate == undefined) {
     return "#a50a51";
-  } else if (chronicDate != "" && vetDate == "") {
+  } else if (chronicDate != undefined && vetDate == undefined) {
     return "#a50a51";
-  } else if (vetDate != "" && chronicDate != "") {
+  } else if (vetDate != undefined && chronicDate != undefined) {
     return "#a50a51";
   }
 }
 
 function setBackgroundColor(vetDate, chronicDate) {
-  if (vetDate == "" && chronicDate == "") {
+  if (vetDate == undefined && chronicDate == undefined) {
     return "rgb(22, 22, 22)";
-  } else if (vetDate != "" && chronicDate == "") {
+  } else if (vetDate != undefined && chronicDate == undefined) {
     return "#a50a51";
-  } else if (chronicDate != "" && vetDate == "") {
+  } else if (chronicDate != undefined && vetDate == undefined) {
     return "#a50a51";
-  } else if (vetDate != "" && chronicDate != "") {
+  } else if (vetDate != undefined && chronicDate != undefined) {
     return "#a50a51";
   }
 }
 
 
 // Draw topline numbers and map
-function draw(state) {
+function draw(state, map, results) {
   // format large numbers with commas
   let format = d3.format(",." + d3.precisionFixed(1) + "f")
 
@@ -208,14 +245,12 @@ function draw(state) {
     })
 
 
-
   // MAP
 
   // Set colorscales
-  let colorScale = d3.scaleOrdinal().domain(["Yes", ""]).range(["#a50a51", "white"]);
-  let textScale = d3.scaleOrdinal().domain(["Yes", ""]).range(["#a50a51", "#ff6f0c"]);
-
-  let fontWeight = d3.scaleOrdinal().domain(["Yes", ""]).range(["600", "300"]);
+  let colorScale = d3.scaleOrdinal().domain(["Yes", undefined]).range(["#a50a51", "white"]);
+  let textScale = d3.scaleOrdinal().domain(["Yes", undefined]).range(["#a50a51", "#ff6f0c"]);
+  let fontWeight = d3.scaleOrdinal().domain(["Yes", undefined]).range(["600", "300"]);
 
   // Assign SVG, make responsive with viewbox
   svg = d3
@@ -241,32 +276,35 @@ function draw(state) {
     .duration(200)
     .style("opacity", "1");
 
+  const radius = 11
+
   // Add dots to SVG and tooltip event
   const dot = svg
     // Create a dot for each community, and move to lat/long position on map
     .selectAll(".circle")
-    .data(state.mapData, d => d.community)
+    .data(map.data, d => d.community)
     .enter()
     .append("circle")
     .attr("class", "dot")
-    .attr("r", "12")
-    .attr("style", "stroke:#a50a51; stroke-width: 3px")
+    .attr("r", radius)
+    .attr("style", "stroke:#a50a51; stroke-width: 3px;")
     .attr("fill", d => colorScale(d['fz_category']))
     .attr("id", function (d, i) {
-      return d.community.replace(/[^A-Z0-9]/ig, "")
+      const id = d.community.replace(/[^A-Z0-9]/ig, "")
+      return id;
     })
     .attr("transform", d => {
       const [x, y] = projection([+d.longitude, +d.latitude]);
       return `translate(${x}, ${y})`;
     })
 
-    // Map mouseover event listener
-    .on('mouseover', function (d) {
+    .on('mouseover', function (event, d) {
+      d3.select(this).raise()
       // change dot
-      d3.selectAll("#" + this.id)
+      d3.selectAll(`#${this.id}`)
         .style("background-color", d => setBackgroundColor(d.vet_fz_date, d.chronic_fz_date))
         .style("color", "white")
-        .attr("r", "18")
+        .attr("r", radius * 1.5)
         .style("cursor", "pointer")
         .attr("opacity", 1);
 
@@ -276,9 +314,9 @@ function draw(state) {
         .attr('class', 'map-tooltip')
         .attr('style', 'position: absolute;')
         .style('background-color', textScale(d['fz_category']))
-        .style('left', (d3.event.pageX + 10) + 'px')
-        .style('top', d3.event.pageY + 'px')
-        .html("<b>" + d.community.replace(" CoC", "").replace(" Regional", "").replace("Countys", "County") + "</b>, " + d.state + " <br><b style='font-size: 12px; font-weight:400;'>" + setSubtitle(d.vet_fz_date, d.chronic_fz_date) + "</b>")
+        .style('left', (event.x + 10) + 'px')
+        .style('top', event.y + 'px')
+        .html(setTooltip(d.community, d.state, d.vet_fz_date, d.chronic_fz_date))
         .style("opacity", 0)
         .transition(d3.easeElastic)
         .duration(200)
@@ -286,12 +324,12 @@ function draw(state) {
     })
 
     // Map mouseout event listener
-    .on('mouseout', function (d) {
+    .on('mouseout', function (event, d) {
       // reset dot
       d3.selectAll("#" + this.id)
         .style("background-color", "transparent")
         .style("color", d => setSubtitleColor(d.vet_fz_date, d.chronic_fz_date))
-        .attr("r", "12")
+        .attr("r", radius)
         .attr("cursor", "default")
         .attr("opacity", 0.8)
       // remove tooltip
@@ -308,7 +346,6 @@ function draw(state) {
       selection
       .attr("opacity", 0)
       .transition(d3.easeElastic)
-      .delay(d => 10 * d.latitude)
       .attr("opacity", 0.8)
     );
 
@@ -327,15 +364,15 @@ function draw(state) {
     .selectAll("text")
     .attr("class", "list-item")
     .attr("opacity", 0.8)
-    .data(state.mapData, d => d.community)
+    .data(map.data, d => d.community)
     .join("text")
     .style("color", d => setSubtitleColor(d.vet_fz_date, d.chronic_fz_date))
     .style("font-weight", d => fontWeight(d['fz_category']))
     .attr("id", function (d, i) {
       return d.community.replace(/[^A-Z0-9]/ig, "")
     })
-    .html(d => d.community.replace(" CoC", "").replace(" Regional", "").replace("Countys", "County") + ",&nbsp <b style='font-weight:400;'>" + d.state + "</b>" + setAsterisk(d.vet_fz_date, d.chronic_fz_date) + "<br>")
-    .on("mouseover", function (d) {
+    .html(d => `${d.community} ,&nbsp <b style='font-weight:400;'> ${d.state} </b> ${setAsterisk(d.vet_fz_date, d.chronic_fz_date)} <br>`)
+    .on("mouseover", function (event, d) {
       d3.selectAll("#" + this.id)
         .style("background-color", d => setBackgroundColor(d.vet_fz_date, d.chronic_fz_date))
         .style("color", "white")
@@ -349,8 +386,8 @@ function draw(state) {
         .append('div')
         .attr('class', 'list-tooltip')
         .attr('style', 'position: absolute;')
-        .style('left', (d3.event.pageX + 15) + 'px')
-        .style('top', (d3.event.pageY + 15) + 'px')
+        .style('left', (event.pageX + 15) + 'px')
+        .style('top', (event.pageY + 15) + 'px')
         .html(setSubtitle(d.vet_fz_date, d.chronic_fz_date))
         .style("opacity", 0)
         .transition(d3.easeElastic)
@@ -379,8 +416,6 @@ function draw(state) {
 
   let elem = document.getElementById('communities');
   let rect = elem.getBoundingClientRect();
-
-  console.log(rect.height);
 
   d3.select(".communities-list")
     .style("height", Math.floor(rect.height) * 0.96 + "px")
